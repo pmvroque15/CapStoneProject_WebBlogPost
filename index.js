bc6cfb807c84f35af6ac362471d7e962cc150649
pmvroque15 <pmvroque@gmail.com>
2025-05-17 02:37:13 -0400

    Initialized the port and started the navbar and footer. Needs more styling but it will be done later. Will focus on the main page and its authentication.

---
+++ index.js
@@ -0,0 +1,22 @@
+import bodyParser from "body-parser";
+import express from "express"
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+   res.render("index.ejs")
+})
+app.listen(port, () => {
+    console.log(`Running in port ${port}`)
+})
