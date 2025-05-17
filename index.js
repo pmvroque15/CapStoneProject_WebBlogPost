03b7be6eea96bbcc0deea28ffd364b9ed5b87830
pmvroque15 <pmvroque@gmail.com>
2025-05-17 11:46:08 -0400

    Started initializing the pages of the website. Added some bootstrap and routed the links on the navbar. Will continue putting up basic bootsrap code just to see more visual and I can troubleshoot it better when I start styling it. The design will be the last priority. Next, I will focus on setting up the fixed authentication logic post request.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import bodyParser from "body-parser";
-import express from "express"
+import express, { Router } from "express"
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { Router } from "express";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,6 +17,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
    res.render("index.ejs")
+})
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs")
 })
 app.listen(port, () => {
     console.log(`Running in port ${port}`)
